7dd9184f0f280c10e69990857d4dff843c40dd3c
gudeqing <822466659@qq.com>
2019-03-21 16:54:30 +0800

    比对参数调整

--- rnaseq_pipeline/rna_tools.py
+++ rnaseq_pipeline/rna_tools.py
@@ -150,7 +150,7 @@ class NestedCmd(Basic):
             self.mkdir(result_dir)
             prefix = os.path.join(result_dir, sample + '.')
             args['outFileNamePrefix'] = prefix
-            args['outSAMattrRGline'] = 'ID:{prefix} SM:{prefix} PL:{platform}'.format(prefix=sample, platform="ILLUMINA")
+            args['outSAMattrRGline'] = 'ID:{prefix} SM:{prefix} PL:{platform}'.format(prefix=sample, platform="illumina")
             cmd = cmdx.star_align(**args)
             commands[step_name + '_' + sample] = self.cmd_dict(
                 cmd=cmd, mem=1024 ** 3 * 10, cpu=2,
@@ -180,7 +180,7 @@ class NestedCmd(Basic):
             self.mkdir(result_dir)
             prefix = os.path.join(result_dir, sample + '.')
             args['outFileNamePrefix'] = prefix
-            args['outSAMattrRGline'] = 'ID:{prefix} SM:{prefix} PL:{platform}'.format(prefix=sample, platform="ILLUMINA")
+            args['outSAMattrRGline'] = 'ID:{prefix} SM:{prefix} PL:{platform}'.format(prefix=sample, platform="illumina")
             cmd = cmdx.star_align(**args)
             commands[step_name + '_' + sample] = self.cmd_dict(
                 cmd=cmd, mem=1024 ** 3 * 10, cpu=2,
