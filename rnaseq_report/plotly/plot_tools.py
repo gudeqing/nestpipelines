8d56a08dc683478a1b040ff4f1a6c1b7ffce5fa7
gudeqing <822466659@qq.com>
2019-05-29 14:07:09 +0800

    hovermode='closest'

--- rnaseq_report/plotly/plot_tools.py
+++ rnaseq_report/plotly/plot_tools.py
@@ -448,6 +448,7 @@ def exp_pca(exp_table, row_sum_cutoff=0.1, exp_cutoff=0.1, cv_cutoff=0.01,
         showlegend=True,
         # width=800,
         # height=600,
+        hovermode='closest',
         xaxis=dict(title='PC1({:.2%})'.format(pc_ratio['PC1'])),
         yaxis=dict(title='PC2({:.2%})'.format(pc_ratio['PC2']))
     )
@@ -964,6 +965,7 @@ def diff_volcano(files: list, outdir='', formats=('html', ), gene_annot=None, li
             showlegend=True,
             xaxis=dict(title='log2FC'),
             yaxis=dict(title='-log10(adjusted_P-value)'),
+            hovermode='closest',
         )
 
         fig = go.Figure(data=[trace, trace2, trace3], layout=layout)
@@ -1027,6 +1029,7 @@ def go_enriched_term_bubble(files: list, top=20, outdir='', formats=('html', ), 
                 title='Top {} enriched {} GO term'.format(top, each),
                 xaxis=dict(title='Enrichment Ratio (ratio_in_study/ratio_in_pop)'),
                 yaxis=dict(dtick=1, tickfont={'size': 8}),
+                hovermode='closest',
             )
             links = []
             for x, y, link in zip(x_data, y_data, df.index):
@@ -1057,6 +1060,7 @@ def kegg_enriched_term_bubble(files: list, top=20, outdir='', formats=('html', )
             df = df0
         gene_number = [int(x.split('/')[0]) for x in df['Ratio_in_study']]
         pval_color = -np.log10(df[p_type])
+        pval_color[pval_color == np.inf] = 10
         pval_color_desc = pval_color.describe()
         pval_color[pval_color > pval_color_desc['75%']*limit] = pval_color_desc['75%']*limit
         score = [round(eval(x)/eval(y), 2) for x, y in zip(df['Ratio_in_study'], df['Ratio_in_pop'])]
@@ -1103,6 +1107,7 @@ def kegg_enriched_term_bubble(files: list, top=20, outdir='', formats=('html', )
             margin=dict(l=350),
             xaxis=dict(title='Enrichment Ratio (ratio_in_study/ratio_in_pop)'),
             yaxis=dict(dtick=1, tickfont={'size': 9}),
+            hovermode='closest',
         )
         links = []
         for x, y, link in zip(x_data, y_data, df['Hyperlink']):
@@ -1117,106 +1122,5 @@ def kegg_enriched_term_bubble(files: list, top=20, outdir='', formats=('html', )
 
 
 if __name__ == '__main__':
-    class Func2Command(object):
-        def __init__(self, callable_dict):
-            self.call(callable_dict)
-
-        @staticmethod
-        def introduce_command(func):
-            import argparse
-            import inspect
-            import json
-            import time
-            import sys
-            if isinstance(func, type):
-                description = func.__init__.__doc__
-            else:
-                description = func.__doc__
-            if '-h' not in sys.argv or '--help' in sys.argv or '-help' in sys.argv:
-                description = None
-            if description:
-                _ = [print(x.strip()) for x in description.split('\n') if x.strip()]
-                parser = argparse.ArgumentParser(add_help=False)
-            else:
-                parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter, description=description)
-            func_args = inspect.getfullargspec(func)
-            arg_names = func_args.args
-            if not arg_names:
-                func()
-                return
-            arg_defaults = func_args.defaults
-            if not arg_defaults:
-                arg_defaults = []
-            arg_defaults = ['None']*(len(arg_names) - len(arg_defaults)) + list(arg_defaults)
-            sig = inspect.signature(func)
-            for arg, value in zip(arg_names, arg_defaults):
-                arg_type = sig.parameters[arg].annotation
-                if arg == 'self':
-                    continue
-                if value == 'None':
-                    if arg_type in [list, tuple, set]:
-                        parser.add_argument('-' + arg, nargs='+', required=True, metavar=arg)
-                    elif arg_type in [int, str, float]:
-                        parser.add_argument('-' + arg, type=arg_type, required=True, metavar=arg)
-                    else:
-                        parser.add_argument('-'+arg, required=True, metavar=arg)
-                elif type(value) == bool:
-                    if value:
-                        parser.add_argument('--'+arg, action="store_false", help='default: True')
-                    else:
-                        parser.add_argument('--'+arg, action="store_true", help='default: False')
-                elif value is None:
-                    if arg_type in [list, tuple, set]:
-                        parser.add_argument('-' + arg, nargs='+', required=False, metavar=arg)
-                    elif arg_type in [int, str, float]:
-                        parser.add_argument('-' + arg, type=arg_type, required=False, metavar=arg)
-                    else:
-                        parser.add_argument('-'+arg, required=False, metavar='Default:' + str(value))
-                else:
-                    if arg_type in [list, tuple, set] or (type(value) in [list, tuple, set]):
-                        default_value = ' '.join(str(x) for x in value)
-                        if type(value) in [list, tuple]:
-                            one_value = value[0]
-                        else:
-                            one_value = value.pop()
-                        parser.add_argument('-' + arg, default=value, nargs='+', type=type(one_value),
-                                            metavar='Default:'+default_value, )
-                    else:
-                        parser.add_argument('-' + arg, default=value, type=type(value),
-                                            metavar='Default:' + str(value), )
-            if func_args.varargs is not None:
-                print("warning: *varargs is not supported, and will be neglected! ")
-            if func_args.varkw is not None:
-                print("warning: **keywords args is not supported, and will be neglected! ")
-            args = parser.parse_args().__dict__
-            try:
-                with open("Argument_detail.json", 'w') as f:
-                    json.dump(args, f, indent=2, sort_keys=True)
-            except IOError:
-                print('Current Directory is not writable, thus argument log is not written !')
-            start = time.time()
-            func(**args)
-            print("total time: {}s".format(time.time() - start))
-
-        def call(self, callable_dict):
-            import sys
-            excludes = ['introduce_command', 'Func2Command']
-            _ = [callable_dict.pop(x) for x in excludes if x in callable_dict]
-            if len(callable_dict) >= 2:
-                if len(sys.argv) <= 1:
-                    print("The tool has the following sub-commands: ")
-                    _ = [print(x) for x in callable_dict]
-                    return
-                sub_cmd = sys.argv[1]
-                sys.argv.remove(sub_cmd)
-
-                if sub_cmd in callable_dict:
-                    self.introduce_command(callable_dict[sub_cmd])
-                else:
-                    print('sub-command: {} is not defined'.format(sub_cmd))
-            elif len(callable_dict) == 1:
-                self.introduce_command(callable_dict.pop(list(callable_dict.keys())[0]))
-
-    callable_dict = {x: y for x, y in locals().items() if callable(y)}
-    _ = [callable_dict.pop(x) for x in {'Func2Command'} if x in callable_dict]
-    Func2Command(callable_dict)
+    from xcmds import xcmds
+    xcmds.xcmds(locals())
