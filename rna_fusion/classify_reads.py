5a144a455ba3e4d624c209bcb32573811554d750
gudeqing <822466659@qq.com>
2019-10-10 14:31:43 +0800

    进一步修改中

--- rna_fusion/classify_reads.py
+++ rna_fusion/classify_reads.py
@@ -18,6 +18,16 @@ import itertools
 import logging
 import difflib
 import statistics
+import time
+
+
+def timer(func):
+    def inner(*args, **kw):
+        start_time = time.time()
+        result = func(*args,**kw)
+        print(f'Runtime of {func.__name__} is', time.time()-start_time, 's')
+        return result
+    return inner
 
 
 def set_logger(name='log.info', logger_id='x'):
@@ -165,16 +175,45 @@ def determine_fusion_type(a, a2, libtype='ISR'):
 
 
 def filter_by_seq_similarity(ref_obj, chr1, break1, chr2, break2, reverse=False,
-                             cutoff=0.8, extend_up=100, extend_down=100):
-    seq = ref_obj.fetch(chr1, break1-extend_up, break1+extend_down)
+                             cutoff=0.8, extend_up=80, extend_down=80):
+    try:
+        seq = ref_obj.fetch(chr1, break1-extend_up, break1+extend_down)
+        seq2 = ref_obj.fetch(chr2, break2-extend_up, break2+extend_down)
+    except KeyError:
+        chr1 = chr1[3:]
+        chr2 = chr2[3:]
+        try:
+            seq = ref_obj.fetch(chr1, break1 - extend_up, break1 + extend_down)
+            seq2 = ref_obj.fetch(chr2, break2 - extend_up, break2 + extend_down)
+        except:
+            return True
+
+    # 通过大小写判断是否在重复区域附近
+    seq_in_repeat = sum(x.islower() for x in seq) / len(seq) >= 0.2
+    seq2_in_repeat = sum(x.islower() for x in seq2) / len(seq2) >= 0.2
+    if seq_in_repeat or seq2_in_repeat:
+        return True
+
+    # 判断两个断点附近的序列是否非常相似或者包含的参考序列N太多
     if reverse:
         seq = reverse_complement(seq)
-    seq2 = ref_obj.fetch(chr2, break2-extend_up, break2+extend_down)
     ratio = difflib.SequenceMatcher(None, seq.lower(), seq2.lower()).ratio()
-    if ratio > cutoff:
+    if ratio > cutoff or seq.upper().count('N') > len(seq)*0.1 or seq2.upper().count('N') > len(seq2)*0.1:
         return True
-    else:
-        return False
+
+
+def filter_by_coverage(bam_obj, chr_name, start, direction='keep_up', extend=23):
+    # 判断断点上游或下游是否有reads覆盖，如果几乎没有read覆盖，则极有可能是一个假的断点
+    try:
+        if direction == 'keep_up':
+            cov = len(list(bam_obj.fetch(chr_name, min([start-extend, 0]), start-3)))
+        else:
+            cov = len(list(bam_obj.fetch(chr_name, start+3, max([start+extend]))))
+        if cov < 4:
+            return True
+    except KeyError:
+        return True
+    return False
 
 
 def junction_from_single_end(splits, ref_fasta, min_intron_size=30):
@@ -384,48 +423,71 @@ def junction_from_one_pair(split_pair_reads, ref_fasta, max_inner_dist=250, libt
     return fusion_paired, fusion_type, single_support
 
 
-def junction_from_two_pairs(split_pair_reads, ref_fasta, min_intro_size=30):
-    ref = pysam.FastaFile(ref_fasta)
-    read_names = [x[:-2] for x in split_pair_reads.keys() if x.endswith('0')]
+@timer
+def junction_from_two_pairs(split_pairs, ref_fasta, bam, min_intro_size=30, r1_on_forward=True):
+    ref_obj = pysam.FastaFile(ref_fasta)
+    read_names = [x[:-2] for x in split_pairs.keys() if x.endswith('0')]
     jac = dict()
     hang_len = 7
     for name in read_names:
         r1_name, r2_name = name + '_0', name + '_1'
-        if (r1_name not in split_pair_reads) or (r2_name not in split_pair_reads):
+        if (r1_name not in split_pairs) or (r2_name not in split_pairs):
             continue
-        for a, a2 in itertools.product(split_pair_reads[r1_name], split_pair_reads[r2_name]):
-            if a.reference_start != a2.next_reference_start:
+        for r1, r2 in itertools.product(split_pairs[r1_name], split_pairs[r2_name]):
+            # 判读是不是r1和r2是不是一对
+            if r1.reference_start != r2.next_reference_start:
                 continue
-            if not a.cigartuples or (not a2.cigartuples):
+            if not r1.cigartuples or (not r2.cigartuples):
                 continue
-            if a.cigartuples[0][0] == 4 and 'S' not in a2.cigarstring:
-                if len(a.seq[:a.query_alignment_start]) >= hang_len:
-                    junction_around = a.seq[a.query_alignment_start-hang_len:a.query_alignment_start+hang_len]
-                    jac.setdefault(junction_around, []).append((a.reference_name, a.reference_start))
-            if a.cigartuples[-1][0] == 4 and 'S' not in a2.cigarstring:
-                if len(a.seq[a.query_alignment_end:]) >= hang_len:
-                    junction_around = a.seq[a.query_alignment_end-hang_len:a.query_alignment_end+hang_len]
-                    jac.setdefault(junction_around, []).append((a.reference_name, a.reference_end))
-            if 'S' not in a.cigarstring and a2.cigartuples[0][0] == 4:
-                if len(a2.seq[:a2.query_alignment_start]) >= hang_len:
-                    junction_around = a2.seq[a2.query_alignment_start-hang_len:a2.query_alignment_start+hang_len]
-                    jac.setdefault(junction_around, []).append((a2.reference_name, a2.reference_start))
-            if 'S' not in a.cigarstring and a2.cigartuples[-1][0] == 4:
-                if len(a2.seq[a2.query_alignment_end:]) >= hang_len:
-                    junction_around = a2.seq[a2.query_alignment_end - hang_len:a2.query_alignment_end + hang_len]
-                    jac.setdefault(junction_around, []).append((a2.reference_name, a2.reference_end))
-
+            # 寻找同链上的融合
+            # 判断r1是不是直接比对到正链，如果不涉及不同链融合，此时要求r2必定是需要反向互补后才能比对到正链
+            if r1.is_reverse != r2.is_reverse == r1_on_forward:
+                if r1.cigartuples[0][0] == 4:
+                    if len(r1.seq[:r1.query_alignment_start]) >= hang_len:
+                        junction_around = r1.seq[r1.query_alignment_start-hang_len:r1.query_alignment_start+hang_len]
+                        jac.setdefault(junction_around, set()).add(
+                            (r1.reference_name, r1.reference_start, r1.seq, 'keep_down', r1.reference_end)
+                        )
+                if r1.cigartuples[-1][0] == 4:
+                    if len(r1.seq[r1.query_alignment_end:]) >= hang_len:
+                        junction_around = r1.seq[r1.query_alignment_end-hang_len:r1.query_alignment_end+hang_len]
+                        jac.setdefault(junction_around, set()).add(
+                            (r1.reference_name, r1.reference_end, r1.seq, 'keep_up', r1.reference_start)
+                        )
+                if r2.cigartuples[0][0] == 4:
+                    if len(r2.seq[:r2.query_alignment_start]) >= hang_len:
+                        junction_around = r2.seq[r2.query_alignment_start-hang_len:r2.query_alignment_start+hang_len]
+                        jac.setdefault(junction_around, set()).add(
+                            (r2.reference_name, r2.reference_start, r2.seq, 'keep_down', r2.reference_end)
+                        )
+                if r2.cigartuples[-1][0] == 4:
+                    if len(r2.seq[r2.query_alignment_end:]) >= hang_len:
+                        junction_around = r2.seq[r2.query_alignment_end - hang_len:r2.query_alignment_end + hang_len]
+                        jac.setdefault(junction_around, set()).add(
+                            (r2.reference_name, r2.reference_end, r2.seq, 'keep_up', r2.reference_start)
+                        )
     fusion_dict = dict()
+    bam_obj = pysam.AlignmentFile(bam)
     for k, v in jac.items():
         if len(v) < 3:
             continue
         for p, p2 in itertools.combinations(v, 2):
-            if p[0] != p2[0] or abs(p[1] - p2[1]) > min_intro_size:
-                if not filter_by_seq_similarity(ref, p[0], p[1], p2[0], p2[1]):
+            if (p[0] != p2[0] or abs(p[1] - p2[1]) > min_intro_size) and p[3] != p2[3]:
+                if filter_by_seq_similarity(ref_obj, p[0], p[1], p2[0], p2[1]):
+                    continue
+                # if filter_by_coverage(bam_obj, p[0], p[4], direction=p[3]):
+                #     continue
+                # if filter_by_coverage(bam_obj, p2[0], p2[4], direction=p2[3]):
+                #     continue
+                if p[3] == 'keep_up':
                     key = p[0] + ':' + str(p[1]) + ' ' + p2[0] + ':' + str(p2[1])
-                    fusion_dict.setdefault(key, 0)
-                    fusion_dict[key] += 1
+                else:
+                    key = p2[0] + ':' + str(p2[1]) + ' ' + p[0] + ':' + str(p[1])
+                fusion_dict.setdefault(key, 0)
+                fusion_dict[key] += 1
+    bam_obj.close()
     return dict(sorted(fusion_dict.items(), key=lambda x: x[1], reverse=True))
+
 
 
 def fusion_pipe(bam_file, ref_fasta, max_inner_dist=300, libtype='ISR'):
