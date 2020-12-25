f3317b62bc937e1cfec3bc537851d9e9509db173
gudeqing <822466659@qq.com>
2020-12-25 14:16:12 +0800

    z再次调为1.96，对应95% conf

--- VcfFilter/vardict_filter.py
+++ VcfFilter/vardict_filter.py
@@ -381,7 +381,7 @@ class VardictFilter():
                     ctrl_af_as_error_rate = True
                     error_rate = normal_af
             # judge = self.pass_seq_error(r, tumor, error_rate, z=1.96, factor=1.2)
-            judge = self.pass_seq_error(r, tumor, error_rate, z=2.58, factor=1)
+            judge = self.pass_seq_error(r, tumor, error_rate, z=1.96, factor=1)
             if not judge[0]:
                 if ctrl_af_as_error_rate:
                     reasons.append('SeqErrorOrGermline')
